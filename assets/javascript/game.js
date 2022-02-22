66acb17e9afcdc590447bffea4745dbc6f798fe2
LemanMiriyeva <leman.miriyeva02@mail.ru>
2022-02-22 15:14:43 +0400

    initial commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Setting for zero
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -8,7 +7,6 @@ var letterUser = [];
 var eachofLetters = null;
 
 
-// Sets the computerGuess variable equal to a random choice from the computerChoice array.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 function countGuessesLeft() {
@@ -27,7 +25,6 @@ var restart = function() {
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-// When the user presses a key, it will run the following function..
 document.onkeyup = function(event) {
 	guessesLeft--;
 
